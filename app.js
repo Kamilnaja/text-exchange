88b1636c1e0574a12ed6085ec847636606998064
Kamil Naja <kamilnaja@gmail.com>
2017-06-10 23:43:53 +0200

    delete works fine

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', function(req, res) {
@@ -23,6 +24,56 @@ app.get('/texts', function (req, res) {
    })
 });
 
+//retrieve single text
+
+app.get('/texts/:id', function (req, res) {
+    let text_id = req.params.id;
+    if (!text_id) {
+        return res.status(400)
+            .send({error: true, message: "enter id"})
+    }
+    connection.query('SELECT * FROM texts where id=?', text_id, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({error: false, data: results[0], message: 'Texts list'})
+    })
+});
+
+//find text by name
+app.get('/texts/search/:keyword', function (req, res) {
+   var keyword = req.params.keyword;
+   connection.query("SELECT * FROM texts WHERE title LIKE ?", ['%' + keyword + '%'], function (error, results, fields) {
+       if (error) throw error;
+       return res.send({error: false, data: results, message: 'Todos search list'});
+   })
+});
+
+//add new text
+
+app.post('/texts', function (req, res) {
+    let text = req.body.text;
+    if (!text) {
+        return res.status(400).send({error:true, message: 'Please provide text'});
+    }
+    connection.query("INSERT INTO text SET ? ", {text: text}, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({error: false, data: results, message: "Added new text !!!"})
+    })
+});
+
+//delete
+
+app.delete('/texts/:id', function (req, res) {
+
+    let text_id = req.params.id;
+
+    if (!text_id) {
+        return res.status(400).send({ error: true, message: 'Please provide text_id' });
+    }
+    connection.query('DELETE FROM texts WHERE id = ?', text_id, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'text has been updated successfully.' });
+    });
+});
 
 app.listen(8080, function () {
     console.log("run at 8080");
--- src/app/textlist/textlist.component.ts
+++ src/app/textlist/textlist.component.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { DataServiceService } from "../data-service.service";
+import {Component, OnInit} from '@angular/core';
+import {DataServiceService} from "../data-service.service";
 
 @Component({
-  selector: 'app-textlist',
-  templateUrl: './textlist.component.html',
+    selector: 'app-textlist',
+    templateUrl: './textlist.component.html',
 })
 export class TextlistComponent implements OnInit {
-  texts:any = [];
-  constructor(private _DataService: DataServiceService) { }
+    texts: any = [];
 
-  ngOnInit() {
-       this._DataService.getTexts()
-      .subscribe(resTextsData => this.texts = resTextsData);
+    constructor(private _DataService: DataServiceService) {
+    }
 
-
-  }
-
-
+    ngOnInit() {
+        this._DataService.getTexts()
+            .subscribe(resTextsData => this.texts = resTextsData);
+    }
 }
